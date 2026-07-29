bd2915c0fc5ee50d52e20561647de781b81b422e
Sondos Aabed <sondosahmedabed@gmail.com>
2026-07-29 18:37:17 +0300

    hide manage app

--- style.py
+++ style.py
@@ -546,5 +546,10 @@ footer {visibility: hidden;}
 header {visibility: hidden;}
 div[data-testid="stDecoration"] {display: none;}
 div[data-testid="stToolbar"] {visibility: hidden;}
+
+/* Hide Streamlit Action Elements & Viewer Badges */
+div[data-testid="stStatusWidget"] { display: none !important; }
+.stDeployButton { display: none !important; }
+div[data-testid="stAppViewBlockContainer"] > footer { display: none !important; }
 </style>
 """
